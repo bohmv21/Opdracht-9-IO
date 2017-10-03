0d1545ad0642095e2a7453a9bf7ac2f3c112d5e8
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-10-03 13:18:21 +0200

    fixed

--- Opdracht 9 IO/Program.cs
+++ Opdracht 9 IO/Program.cs
@@ -15,7 +15,7 @@ namespace Opdracht_9_IO
         {
             string strFileName = "data.txt";
             string strCMD = "Ms.txt";
-            string strTextPath = @"C:\Users\bohmv\Documents\ICT shit\Visual Studio\Opdracht 9 IO\Opdracht 9 IO\bin\Debug\Recources\";
+            //string strTextPath = @"C:\Users\bohmv\Documents\ICT shit\Visual Studio\Opdracht 9 IO\Opdracht 9 IO\bin\Debug\Recources\";
             string strPath = @"C:\Users\bohmv\Documents\ICT shit\Visual Studio\Opdracht 9 IO\Opdracht 9 IO\Text spam\";
             //string strInput = "Dit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regel ";
 
@@ -24,19 +24,19 @@ namespace Opdracht_9_IO
             var msg = File.ReadAllLines(strFileName);
             Console.WriteLine("Content uit een bestand : " + strPath + strFileName);
             Console.WriteLine("Hoeveel lines wilt u displayen");
-            int intLines = 95;
+            int intAmountOfDocs = 95;
             // Convert.ToInt16(Console.ReadLine());
             string strMsg = File.ReadAllText(strCMD);
             Console.WriteLine(strMsg);
 
             Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
-            //
+            
             do
             {
                 intCount++;
                 File.WriteAllLines(strPath + intCount + ".txt", msg);
 
-            } while (intCount < intLines);
+            } while (intCount < intAmountOfDocs);
         }
     }
 }
