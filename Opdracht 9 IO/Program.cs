3a0458314ca3b9c5bff0d034d779f91906b694eb
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2017-10-04 10:47:31 +0200

    Restored the merge fuckup

--- Opdracht 9 IO/Program.cs
+++ Opdracht 9 IO/Program.cs
@@ -15,31 +15,35 @@ namespace Opdracht_9_IO
         {
             string strFileName = "data.txt";
             string strCMD = "Ms.txt";
+            //string strTextPath = @"C:\Users\bohmv\Documents\ICT shit\Visual Studio\Opdracht 9 IO\Opdracht 9 IO\bin\Debug\Recources\";
             string strPath = "../";
+            //string strInput = "Dit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regelDit is tekst uit bestand op regel ";
 
 
             int intCount = 1;
             var msg = File.ReadAllLines(strFileName);
+            Console.WriteLine("Content uit een bestand : " + strPath + strFileName);
+            Console.WriteLine("Hoeveel lines wilt u displayen");
             int intAmountOfDocs = 1000;
-
+            // Convert.ToInt16(Console.ReadLine());
             string strMsg = File.ReadAllText(strCMD);
             Console.WriteLine(strMsg);
 
             Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
-
-
-            int beep = 1000;
+            //Piepjescode
+            /*
+            int beep = 50;
 
             do
             {
-                Console.Beep(beep , 50);
-                beep = beep + 50;
+                Console.Beep(beep , 100);
+                beep = beep + 100;
                 if (beep >= 13000)
                 {
                     do
                     {
-                        Console.Beep(beep, 50);
-                        beep = beep - 50;
+                        Console.Beep(beep, 100);
+                        beep = beep - 100;
                     } while (beep >= 150);
                 }
             } while (true);
@@ -47,26 +51,34 @@ namespace Opdracht_9_IO
 
             if (!File.Exists("filecheck.dat"))
             {
-                    File.WriteAllText("filecheck.dat", "1");
+                File.WriteAllText("filecheck.dat", "1");
             }
 
 
             string strFileCheck = File.ReadAllLines("filecheck.dat").Last();
             int intFileCheck = Convert.ToInt32(strFileCheck);
             intFileCheck += intAmountOfDocs;
-            if (intFileCheck < intAmountOfDocs-1)
+            if (intFileCheck < intAmountOfDocs - 1)
             {
+
+            }
+            else
+            {
+                intAmountOfDocs = intFileCheck;
+            }
+            File.WriteAllText("filecheck.dat", Convert.ToString(intAmountOfDocs));
+            Console.ReadLine();
 
 
 
             //Dit is voor loop1, er moet nog een 2e loop gemaakt worden die alleen activeert zodra dat de files al bestaan.
-                do
-                {
-                    File.WriteAllLines(strPath + intCount + ".txt", msg);
-                    intCount++;
+            do
+            {
+                File.WriteAllLines(strPath + intCount + ".txt", msg);
+                intCount++;
 
-                } while (intCount < intAmountOfDocs);
-            }
+            } while (intCount < intAmountOfDocs);
         }
     }
+}
 
